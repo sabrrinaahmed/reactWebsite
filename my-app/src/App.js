50ce6599191588630d296fb72fa229e649664c48
sabrrinaahmed <sabrrinaahmed@tamu.edu>
2021-05-09 18:01:50 -0500

    added menu and buttons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ my-app/src/components/navBar.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    return (
+        <>
+        <nav className='navBar'>
+            <div className='navBar-container'>
+                <Link to="/" className='navBar-logo'>
+                    SABS <i class="fas fa-server"></i>
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/aboutMe' className='nav-links' onClick={closeMobileMenu}>
+                            About Me
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/profImage' className='nav-links' onClick={closeMobileMenu}>
+                            Professional Image
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/portfolio' className='nav-links' onClick={closeMobileMenu}>
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/service' className='nav-links' onClick={closeMobileMenu}>
+                            Service
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default NavBar
+
+
+{/* uses font awesome CDN for fonts */}
