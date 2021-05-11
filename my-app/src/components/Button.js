c82ee5e4cf28ddb42e4ad705cc6a4371925a6dd1
sabrrinaahmed <sabrrinaahmed@tamu.edu>
2021-05-10 21:57:12 -0500

    finised button

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -10,17 +10,15 @@ export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-    return {
-        // <Link to='/sign-up' className='btn-mobile'>
-        //     <button
-        //     className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-        //     onClick={onClick}
-        //     type={type}
-        //     >
-        //         {children}
-        //     </button>
-
-        //     <button>Get Started</button>
-        // </Link>
-    }
+    return (
+        <Link to='/sign-up' className='btn-mobile'>
+          <button
+            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}
+          >
+            {children}
+          </button>
+        </Link>
+      );
 }
